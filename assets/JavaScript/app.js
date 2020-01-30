e04a31c2fc56b7104cc1d0e72d99c460162a05f8
Andrew Weiss <wandrew8@gmail.com>
2020-01-30 15:36:12 -0800

    return home button added and skills updated

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -141,3 +141,20 @@ function obCallback(card) {
         }
     })
 }
+
+//Scroll to top of page button
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
